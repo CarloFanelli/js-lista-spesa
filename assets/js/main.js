c48333c4f940565e983d20ff93f154f746da8408
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-08-30 15:09:28 +0200

    add control for items

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che 
 
 
 
-const listaSpesa = ['latte','uova','burro','zucchero','pasta','caramelle'];
+const listaSpesa = ['latte', 'uova', 'burro', 'zucchero', 'pasta', 'caramelle'];
 
 // variabile per il ciclo while
 let i = 0;
@@ -15,45 +15,50 @@ let i = 0;
 const listaSpesaDOM = document.getElementById('listaSpesa');
 
 // ciclo while
-while (i < listaSpesa.length){
+while (i < listaSpesa.length) {
 
     const prodottoSpesa = listaSpesa[i];
     // console.log(prodottoSpesa); -- debug
 
     const lisataMarkup = `<li>${prodottoSpesa}</li>`;
 
-    listaSpesaDOM.insertAdjacentHTML('beforeend',lisataMarkup);
+    listaSpesaDOM.insertAdjacentHTML('beforeend', lisataMarkup);
 
     i++;
 }
 
-document.getElementById('aggiornaSpesa').addEventListener('click',function(e){
+document.getElementById('aggiornaSpesa').addEventListener('click', function (e) {
     e.preventDefault();
 
-    while (i < listaSpesa.length){
+    while (i < listaSpesa.length) {
 
         const prodottoSpesa = listaSpesa[i];
         // console.log(prodottoSpesa); -- debug
-    
+
         const lisataMarkup = `<li>${prodottoSpesa}</li>`;
-    
-        listaSpesaDOM.insertAdjacentHTML('beforeend',lisataMarkup);
-    
+
+        listaSpesaDOM.insertAdjacentHTML('beforeend', lisataMarkup);
+
         i++;
     }
 })
 
-document.getElementById('aggiungiElemento').addEventListener('click' , function(e){
+document.getElementById('aggiungiElemento').addEventListener('click', function (e) {
     e.preventDefault();
 
     const nuovoProdotto = document.getElementById('prodotto').value;
     // console.log(nuovoProdotto); -- debug
 
-    listaSpesa.push(nuovoProdotto);
+    if (isNaN(nuovoProdotto)) {
+        listaSpesa.push(nuovoProdotto);
 
-    console.log(listaSpesa);
+        console.log(listaSpesa);
 
-    document.getElementById('prodotto').value = '';
+        document.getElementById('prodotto').value = '';
+    } else {
+
+        console.log('ciao');
+    }
 
 }
 )
