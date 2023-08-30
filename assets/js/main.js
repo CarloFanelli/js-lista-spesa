5ed8bb627c579bbd64cee17dde94da9ffc61e87d
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-08-30 13:48:38 +0200

    add while loop and print on screen the list

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,27 @@
 Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 */
 
+
+
+const listaSpesa = ['latte','uova','burro','zucchero','pasta','caramelle'];
+
+// variabile per il ciclo while
+let i = 0;
+
+// console.log(listaSpesa.length);  -debug
+
+//collego elemento del DOM
+const listaSpesaDOM = document.getElementById('listaSpesa');
+
+// ciclo while
+while (i < listaSpesa.length){
+
+    const prodottoSpesa = listaSpesa[i];
+    // console.log(prodottoSpesa); -- debug
+
+    const lisataMarkup = `<li>${prodottoSpesa}</li>`;
+
+    listaSpesaDOM.insertAdjacentHTML('beforeend',lisataMarkup);
+
+    i++;
+}
